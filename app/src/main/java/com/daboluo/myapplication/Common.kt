af87fc404bd918a82413e26251740f29aa0837fb
Zheng shaomin <daboluo719@gmail.com>
2025-02-21 15:15:26 +0800

    second

---
+++ app/src/main/java/com/daboluo/myapplication/Common.kt
@@ -0,0 +1,9 @@
+package com.daboluo.myapplication
+
+/**
+ *作者：daboluo on 2025/2/21 15:13
+ *Email:daboluo719@gmail.com
+ */
+object Common {
+    const val SERVER_URL = "ws://10.0.2.2:8000"
+}
--- app/src/main/java/com/daboluo/myapplication/MainActivity.kt
+++ app/src/main/java/com/daboluo/myapplication/MainActivity.kt
@@ -13,7 +13,6 @@ import org.json.JSONObject
 import java.net.URI
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var socket: MyWebSocketClient
 
     private lateinit var webSocketManager: WebSocketManager
     private lateinit var editText: EditText
@@ -26,9 +25,8 @@ class MainActivity : AppCompatActivity() {
         editText = findViewById(R.id.editText)
 
         // 初始化WebSocket连接
-        val serverUrl = "ws://10.0.2.2:8000"  // 模拟器访问本地用10.0.2.2
         val androidId = "android123"  // 每个客户端唯一ID
-        webSocketManager = WebSocketManager(serverUrl, androidId) {
+        webSocketManager = WebSocketManager(Common.SERVER_URL, androidId) {
             editText.text.toString()
         }
         webSocketManager.connect()
